1b5646029e52e94256372324828ab91b7e3fde68
victornavia <victornavia39@gmail.com>
2022-09-07 00:00:20 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,138 @@
+<!doctype html>
+<html lang="es">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+
+		<!-- Estilos -->
+		<link rel="stylesheet" type="text/css" href="css/estilos.css">
+
+		<!--Ícono-->
+		<link rel="shortcut icon" type="image/x-icon" href="img/ico.ico" />
+
+		<title></title>
+	</head>
+  	<body>
+
+			<div class="container">
+
+				<h1>Drogas farmaceúticas</h1>
+
+				<p>Una droga farmaceútica en el campo médico es la que establece que es toda sustancia química usada para la prevención, diagnóstico o tratamiento de las enfermedades del hombre.</p>
+				<p>Estas son algunas de las drogas farmaceúticas que existen hoy en día:</p>
+
+				<button class="accordion">Nicotina</button>
+				<div class="panel">
+				  	<p>La nicotina es un compuesto orgánico, un alcaloide encontrado principalmente en la planta del tabaco (Nicotiana tabacum), con alta concentración en sus hojas (constituye cerca del 5% del peso de la planta y del 3% del peso del tabaco seco) estando también presente en otras plantas de la familia Solanacea aunque de forma marginal (en el rango de 2–7 µg/kg), como en el caso del tomate, la berenjena, el pimiento y la patata. En cantidades aún más marginales, ha sido encontrada en otras plantas como la coliflor, la pimienta verde o el té negro. Se sintetiza en las zonas de mayor actividad de las raíces de las plantas del tabaco y es transportada por la savia a las hojas verdes. El depósito se realiza en forma de sales de ácidos orgánicos</p>
+				</div>
+
+				<button class="accordion">Opiáceo</button>
+				<div class="panel">
+				  	<p>Es una sustancia que se usa para tratar el dolor o causar sueño. Los opiáceos se elaboran con opio o contienen opio. Los opiáceos se unen con los receptores de opioides del sistema nervioso central. La codeína, la heroína y la morfina son ejemplos de opiáceos. Un opiáceo es un tipo de analgésico.</p>
+				</div>
+
+				<button class="accordion">Benzodiacepinas</button>
+				<div class="panel">
+				  	<p>Las benzodiacepinas son medicamentos que disminuyen la excitación neuronal y que tienen un efecto antiepiléptico, ansiolítico, hipnótico y relajante muscular. Se utilizan como tratamiento de:
+					<ul>
+						<li>Ansiedad generalizada.</li>
+						<li>Insomnio.</li>
+						<li>Fobias.</li>
+						<li>Trastorno obsesivo compulsivo.</li>
+						<li>Trastornos afectivos.</li>
+						<li>Esquizofrenia.</li>
+					</ul>
+
+					Ciertas urgencias psiquiátricas, como la agitación psicomotriz, el estrés ambiental o los trastornos de la personalidad.
+					Delirium tremens, para prevenir la agitación y las crisis convulsivas.
+					“Incluso se han utilizado en otros campos de la medicina, como enfermedades crónicas estresantes, cáncer, alteraciones cardiovasculares o reumáticas”, explica José María Vázquez, vocal por Cataluña de la Sociedad Española de Patología Dual (SEPD).</p>
+				</div>
+
+				<button class="accordion">Medicamentos para tratar el TDAH</button>
+				<div class="panel">
+				  	<p>Para la mayoría de los niños, los medicamentos con estimulantes son una forma segura y eficaz de aliviar los síntomas del trastorno por déficit de atención e hiperactividad (TDAH).  Así como los anteojos ayudan a las personas en enfocar sus ojos para ver, estos medicamentos ayudan a los niños con TDAH a enfocar sus pensamientos mejor y a ignorar distracciones. Esto les ayuda a poner mejor atención y a controlar su comportamiento.<br>
+					Los estimulantes se pueden usar solos o combinados con terapia de conducta Los estudios demuestran que cerca de un 80% de los niños con TDAH que son tratados con estimulantes mejoraran de gran manera una vez que se determina el medicamento y la dosis correcta.</p>
+				</div>
+
+				<button class="accordion">Ambien</button>
+				<div class="panel">
+				  	<p>Es un medicamento usado para tratar el insomnio (incapacidad de dormir) y la ansiedad. Es un tipo de imidazopiridina (sedante hipnótico). También se llama zolpidem.</p>
+				</div>
+
+				<button class="accordion">Esteroides anabólicos</button>
+				<div class="panel">
+				  	<p>"Esteroides anabólicos" es el nombre familiar de unas substancias sintéticas relacionadas a las hormonas sexuales masculinas (por ejemplo, la testosterona). Promueven el crecimiento del músculo esquelético (efectos anabólicos) y el desarrollo de características sexuales masculinas (efectos androgénicos) tanto en hombres como en mujeres. Se utilizará el término "esteroides anabólicos" en este informe debido a su familiaridad, aunque el término correcto de estos compuestos es esteroides "anabólico-androgénicos".</p>
+				</div>
+
+				<button class="accordion">Alprazolam</button>
+				<div class="panel">
+				  	<p>El alprazolam es un fármaco que pertenece a la familia de las benzodiazepinas y se utiliza para el tratamiento de los estados de ansiedad, especialmente en las crisis de angustia, agorafobia, ataques de pánico y estrés intenso. Se vende con nombre genérico y diferentes nombres comerciales, entre ellos Trankimazin, Tranquinal y Xanax. Tiene un efecto hipnótico (favorecedor del sueño a corto plazo) y actúa disminuyendo la excitación del cerebro. Aunque inicialmente se le atribuyeron propiedades antidepresivas, los estudios que lo indicaban eran heterogéneos y de baja calidad, por lo que no está indicado para tratar la depresión. Sin embargo, sí puede ser eficaz para el tratamiento de la ansiedad que se asocia a los procesos depresivos. El alprazolam tiene propiedades sedantes, hipnóticas y anticonvulsivas, pero el efecto más notable es el ansiolítico. Sin embargo, debido a su potencial adictivo y el desarrollo de tolerancia tras un período relativamente breve, de pocas semanas, se recomienda su administración solo en tratamientos de corta duración. Se trata de una benzodiazepina de alta potencia y de eliminación rápida (semivida breve) y este factor se ha relacionado con una mayor gravedad del síndrome de abstinencia cuando se suspende el tratamiento.</p>
+				</div>
+
+				<button class="accordion">Lorazepam</button>
+				<div class="panel">
+				  	<p>El lorazepam, vendido bajo el nombre comercial Orfidal o Ativan, es un fármaco perteneciente al grupo de las benzodiazepinas de alta potencia que tiene las cinco propiedades intrínsecas de este conjunto: ansiolítico, amnésico, sedante e hipnótico, anticonvulsivo y relajante muscular.</p>
+				</div>
+
+				<button class="accordion">Diazepam</button>
+				<div class="panel">
+				  	<p>El diazepam es un fármaco derivado de la benzodiazepina que actúa como modulador alostérico positivo de los receptores GABAA con propiedades ansiolíticas, miorrelajantes, anticonvulsivantes y sedantes.<br>
+					El diazepam se utiliza para tratar estados de ansiedad y está considerada como la benzodiazepina más efectiva para el tratamiento de espasmos musculares. Es una de las benzodiazepinas más frecuentemente administradas tanto a pacientes ambulatorios como ingresados.</p>
+				</div>
+
+				<button class="accordion">Clonazepam</button>
+				<div class="panel">
+				  	<p>El clonazepam es un fármaco perteneciente al grupo de las benzodiazepinas que actúa sobre el sistema nervioso central, con propiedades ansiolíticas, anticonvulsionantes, miorrelajantes, sedantes, hipnóticas y estabilizadoras del estado de ánimo. Se utiliza como antiepiléptico, es particularmente útil en el tratamiento de crisis de ausencia y ausencias atípicas, y altamente efectivo en cualquier desorden de ansiedad, aunque se recomienda no extender su uso a más de 30 días. Se comercializa, entre otros, bajo los nombres de Coquan, Clonagin, Clonex, Diocam, Klonopin, Kriadex, Linotril, Neuryl, Paxam, Rivotril y Zatrix.<br>
+					Tiene un leve efectoansiolítico e hipnótico, también como estabilizador del estado de ánimo a corto plazo, causa amnesia anterógrada.<br>
+					En dosis muy altas, o altas con poca tolerancia, causa una fuerte amnesia pudiendo ser total a corto plazo (dependiendo de la vida media de la benzodiazepina sobredosificada), con desinhibición conductual, provocando alta probabilidad de accidentes graves y actividades riesgosas/inesperadas, aunque una sobredosis puede causar esta amnesia por algunos días, si se consume únicamente benzodiazepina es casi imposible la muerte por sobredosis, en caso de usarse con otras drogas, el riesgo de muerte por sobredosis es mucho más considerable.</p>
+				</div>
+
+				<button class="accordion">Alcohol</button>
+				<div class="panel">
+				  	<p>El alcohol sigue siendo la droga más consumida, el doctor Ávila ha comentado los efectos que produce: lesiones estructurales del cerebro que deriva en dependencia, intoxicación que puede llegar a coma etílico, trastornos de memoria, atrofia cerebral, demencia, crisis convulsivas e incluso provoca confusión, las personas llegan a no saber datos básicos del día a día.</p>
+				</div>
+
+				<button class="accordion">Cocaina</button>
+				<div class="panel">
+				  	<p>La cocaína provoca: lesiones estructurales del cerebro que deriva en dependencia, intoxicación, trastornos de memoria, atrofia cerebral, demencia, crisis convulsivas e incluso provoca confusión además de lesiones nasales, hiperactividad, golpes de calor, hemorragias cerebrales e infartos cerebrales. Los efectos de esta droga pueden llegar a provocar cuadros delirantes y paranoides en las personas que lo consumen.</p>
+				</div>
+
+				<button class="accordion">Cannabis</button>
+				<div class="panel">
+				  	<p>El cannabis tiene efectos irreparables como el déficit de memoria, apatía o cuadros psicóticos. Con respecto a la propuesta de Ciudadanos de legalizar el consumo terapéutico del cannabis, José Juan Ávila considera que no es apropiado pero si se legaliza, debería ser de productos farmacéuticos que son derivados del cannabis para que el grado de dependencia sea menor.</p>
+				</div>
+
+				<button class="accordion">Drogas diseño</button>
+				<div class="panel">
+				  	<p>Cada vez existen más drogas llamadas de diseño, el psiquiatra Ávila alerta de las consecuencias a través de un ejemplo: un error en la fabricación de éxtasis provocó que las personas que lo consumieron desarrollasen Parkinson muy jóvenes.</p>
+				</div>
+			</div>
+
+			<script>
+			var acc = document.getElementsByClassName("accordion");
+			var i;
+
+			for (i = 0; i < acc.length; i++) {
+			  	acc[i].addEventListener("click", function() {
+			    	this.classList.toggle("active");
+			    	var panel = this.nextElementSibling;
+			    	if (panel.style.display === "block") {
+			      	panel.style.display = "none";
+			    	} else {
+			      	panel.style.display = "block";
+			    	}
+			  	});
+			}
+			</script>
+
+    		<!-- Optional JavaScript -->
+    		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    		<script src="js/jquery-3.3.1.slim.min.js"></script>
+    		<script src="js/popper.min.js"></script>
+    		<script src="js/bootstrap.min.js"></script>
+  	</body>
+</html>
